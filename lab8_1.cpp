1b84e7daf1b9acc71139302c7e9aec067d251670
Anukul <anukul_thienkasemsuk@hotmail.com>
2018-01-12 12:26:11 +0700

    initial commit

---
+++ lab8_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+        int arr[10] = {3,2,5,5,6,9,1,3,2,7};
+
+        // เขียนโปรแกรมเพื่อแสดงค่าใน arr
+
+        return 0;
+}
---
+++ lab8_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+        char name[] = {"Anukul"};
+        int nameLength = 6;
+        cout << name;
+
+        // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
+        // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
+
+
+
+        return 0;
+}
