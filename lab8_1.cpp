c2ff328487c3bbd47aa473d3fbcd7d577b88ad82
Unknown <milkarm27@gmail.com>
2018-01-18 22:32:13 +0700

    แก้

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -7,6 +7,16 @@ int main () {
         int arr[10] = {3,2,5,5,6,9,1,3,2,7};
 
         // เขียนโปรแกรมเพื่อแสดงค่าใน arr
+        cout<<arr[0];
+        cout<<arr[1];
+        cout<<arr[2];
+        cout<<arr[3];
+        cout<<arr[4];
+        cout<<arr[5];
+        cout<<arr[6];
+        cout<<arr[7];
+        cout<<arr[8];
+        cout<<arr[9];
 
         return 0;
 }
